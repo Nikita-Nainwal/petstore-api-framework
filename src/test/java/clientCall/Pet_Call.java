5cd3929e89d26774b73dd2c32a0e65d239d00a59
Nikita <nikitanainwal1997@gmail.com>
2025-04-06 11:09:07 +0100

    checking for test file

--- src/test/java/clientCall/Pet_Call.java
+++ src/test/java/clientCall/Pet_Call.java
@@ -2,13 +2,14 @@ package clientCall;
 
 import config.Config;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.annotations.Test;
 import pojo.Pet.AddPet;
 
 public class Pet_Call {
     //Create a request and get a response for the APIs
     //Post Put Delete Get add pet with form data
+    @Test
     public Response postPet (AddPet addPet) {
         return RestAssured.given().baseUri(Config.BASE_URI).contentType("application/json").
         accept("application/json").body(addPet).when().post();
