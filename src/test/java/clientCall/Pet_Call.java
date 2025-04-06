99f1aa7268451ea6dbdc612bc25e548e4451712b
Nikita <nikitanainwal1997@gmail.com>
2025-04-06 21:55:48 +0100

    Post request for multipart and formdata

--- src/test/java/clientCall/Pet_Call.java
+++ src/test/java/clientCall/Pet_Call.java
@@ -3,16 +3,35 @@ package clientCall;
 import config.Config;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import pojo.Pet.AddPet;
 
-public class Pet_Call {
+import java.io.File;
+
+public class  Pet_Call {
     //Create a request and get a response for the APIs
     //Post Put Delete Get add pet with form data
-
+    static final RequestSpecification request = RestAssured.given().baseUri(Config.BASE_URI).accept("application/json");
     public static Response postPet (AddPet addPet) {
-        return RestAssured.given().baseUri(Config.BASE_URI).contentType("application/json").
-        accept("application/json").body(addPet).when().post();
+        return request.contentType("application/json").body(addPet).when().post();
     };
 
+    public static Response postPhoto(AddPet addPet) {
+       return request.contentType("multipart/form-data")
+               .multiPart("file", new File("D:\\Projects\\petstore_APIs\\src\\main\\resources\\petphoto.jpg"))
+               .formParam("additionalMetadata", "adding pet image")
+               .when().post("/"+addPet.getId()+"/uploadImage");
+    }
+
+    public static Response postFormData() {
+        //Avoid using same RequestSpecification between: multipart uploads and form-based submissions
+       return RestAssured.given().baseUri("https://petstore.swagger.io/v2")
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("name" , "Mini")
+                .formParam("status", "unavailable")
+                .when().post("/pet/102");
+
+    }
 
 }
+
--- src/test/java/pet_testCases/Add_Update_pet.java
+++ src/test/java/pet_testCases/Add_Update_pet.java
@@ -3,6 +3,7 @@ package pet_testCases;
 import clientCall.Pet_Call;
 import factory.Post_Put_Pet;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.core.net.Priority;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pojo.Pet.AddPet;
@@ -10,11 +11,12 @@ import utils.JSONUtils;
 
 public class Add_Update_pet {
     Post_Put_Pet createPet = new Post_Put_Pet();
-    @Test
+    AddPet addPet= createPet.createPet();
+
+    @Test(priority = 1)
     public void addPet() {
        //Add asking from user
         // in another test from excel
-        AddPet addPet= createPet.createPet();
         JSONUtils.printPayload(addPet);
         Response response = Pet_Call.postPet(addPet);
         AddPet res = response.as(AddPet.class);
@@ -24,12 +26,28 @@ public class Add_Update_pet {
             Assert.assertEquals(response.jsonPath().getString("name"), "Mini");
             Assert.assertEquals(response.jsonPath().getString("category.name"), "dog");
             Assert.assertEquals(response.jsonPath().getString("tags[0].name"), "Mini");
-            Assert.assertEquals(response.jsonPath().getString("tags[1].name"), "Mino");
+            Assert.assertEquals(response.jsonPath().getString("tags[1].name"), "Mini");
             System.out.println("Pet Added Successfully");
         }
         catch (Exception e) {
             System.out.println("Pet Added Failed, please check the logs");
             e.printStackTrace();
         }
+    }
+    @Test(priority = 2)
+    public void uploadPetImage() {
+         Response response = Pet_Call.postPhoto(addPet);
+         Assert.assertEquals(response.getStatusCode(), 200);
+         String s = response.jsonPath().getString("message");
+         Assert.assertEquals(s.contains("File uploaded "),true);
+    }
+
+    @Test(priority = 3)
+    public void updatePetFormData() {
+        Response respose = Pet_Call.postFormData();
+        System.out.println(respose.jsonPath().getString("message"));
+        System.out.println(respose.jsonPath().getString("code"));
+        System.out.println(respose.jsonPath().getString("type"));
+
     }
 }
